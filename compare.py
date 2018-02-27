941557e64ec965c012642ca4ba05238b9058e16a
davidhuser <dhuser@baosystems.com>
2018-02-27 16:03:11 +0200

    arguments for level & type, prettyprint

--- compare.py
+++ compare.py
@@ -36,6 +36,22 @@ def parse_args():
                         help="Country string"
                         )
 
+    parser.add_argument('--level',
+                        dest='level',
+                        action='store',
+                        required=True,
+                        choices={'psnu', 'site'},
+                        help="Level"
+                        )
+
+    parser.add_argument('--type',
+                        dest='type',
+                        action='store',
+                        required=True,
+                        choices={'normal', 'hts'},
+                        help="Type"
+                        )
+
     return parser.parse_args()
 
 
@@ -53,40 +69,43 @@ def file_identifier(file_path):
 
 def detailed(df1, df2, paths):
     df = df1.merge(df2, how='outer', indicator=True)
-    print(df.copy().rename(columns={'attributeoptioncombo': 'aoc', 'categoryoptioncombo': 'coc'}))
+    with pd.option_context('display.max_rows', 30, 'display.max_columns', 5):
+        print(df.copy().rename(columns={
+            'attributeoptioncombo': 'aoc',
+            'categoryoptioncombo': 'coc',
+            'supportType': 'supType'
+        }))
     now = datetime.now().strftime('%F-%H%M%S')
     filename = "{}_diff_{}.csv".format('_'.join(paths).replace('/', '_'), now)
     pd.DataFrame.to_csv(df, filename)
     print("Saved to {}{}{}".format(color.BOLD, filename, color.END))
 
 
-def compare(dir_paths, country, levels, types):
-    for level in levels:
-        for typ in types:
-            comparable_files = [get_path(f, country, level, typ) for f in dir_paths]
-            path1 = comparable_files[0]
-            filename1 = file_identifier(path1)
-            path2 = comparable_files[1]
-            filename2 = file_identifier(path2)
+def compare(dir_paths, country, level, typ):
+        comparable_files = [get_path(f, country, level, typ) for f in dir_paths]
+        path1 = comparable_files[0]
+        filename1 = file_identifier(path1)
+        path2 = comparable_files[1]
+        filename2 = file_identifier(path2)
 
-            with open(path1, 'r') as jf:
-                data = json.dumps(json.load(jf).get('data'))
-                df1 = pd.read_json(data)
+        with open(path1, 'r') as jf:
+            data = json.dumps(json.load(jf).get('data'))
+            df1 = pd.read_json(data)
 
-            with open(path2, 'r') as jf:
-                data = json.dumps(json.load(jf).get('data'))
-                df2 = pd.read_json(data)
+        with open(path2, 'r') as jf:
+            data = json.dumps(json.load(jf).get('data'))
+            df2 = pd.read_json(data)
 
-            equal = pd.DataFrame.equals(df1, df2)
-            c = color.RED if not equal else color.BOLD
-            print("Comparing {0}{2}{1} with {0}{3}{1} - equal: {0}{4}{1}".format(c,
-                                                                                 color.END,
-                                                                                 filename1,
-                                                                                 filename2,
-                                                                                 equal))
-            if not equal:
-                print("{0}LEFT/DF1:{2}{1} - {0}RIGHT/DF2:{3}{1}".format(color.BOLD, color.END, filename1, filename2))
-                detailed(df1, df2, dir_paths)
+        equal = pd.DataFrame.equals(df1, df2)
+        c = color.RED if not equal else color.BOLD
+        print("Comparing {0}{2}{1} with {0}{3}{1} - equal: {0}{4}{1}".format(c,
+                                                                             color.END,
+                                                                             filename1,
+                                                                             filename2,
+                                                                             equal))
+        if not equal:
+            print("{0}LEFT/DF1:{2}{1} - {0}RIGHT/DF2:{3}{1}".format(color.BOLD, color.END, filename1, filename2))
+            detailed(df1, df2, dir_paths)
 
 
 if __name__ == '__main__':
@@ -94,6 +113,6 @@ if __name__ == '__main__':
     compare(
         dir_paths=[args.folder1, args.folder2],
         country=args.country,
-        levels=['psnu', 'site'],
-        types=['normal', 'hts']
+        level=args.level,
+        typ=args.type
     )
